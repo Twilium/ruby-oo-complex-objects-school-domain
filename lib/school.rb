78c79f1c4b5c6d77b3955e6995c85c059d9c0349
Emmanuel <rivera128@live.com>
2020-11-09 09:12:03 -0600

    commit tree c3c90398de586fb07602f1392db7c1bfbeef0b43

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+    attr_accessor :roster
+
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        # if roster.has_key?(grade)
+        #     roster[grade] << name
+            
+        # else
+        #     roster[grade] = [name]
+        # end
+
+        roster[grade] ||= []
+        roster[grade] << name
+
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        roster.each do |key, value|
+            value.sort!
+        end
+        roster.sort.to_h
+    end
+end
